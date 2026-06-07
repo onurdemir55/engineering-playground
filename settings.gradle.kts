81bbecfe4a681c15e2257fb2d59612cb4ac07902
onur.demir <onurdemir55@gmail.com>
2026-06-07 23:38:25 +0200

    .

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ rootProject.name = "engineering-playground"
 
 include("java-core:collections-demo")
 include("java-core:concurrency-demo")
+include("java-core:language-features-demo")
 include("kafka:producer-consumer-demo")
 include("spring-boot:rest-api-demo")
 include("spring-boot:mongodb-demo")
